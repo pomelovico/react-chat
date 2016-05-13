85a85cf74732a43882ae2ad05b4ed64fd06fd85e
pomelo <lwxvero@qq.com>
2016-05-13 15:36:20 +0800

    react-im

--- src/js/containers/chatroom.js
+++ src/js/containers/chatroom.js
@@ -1,3 +1,12 @@
 /**
  * Created by LikoLu on 2016/5/13.
  */
+import React,{Component} from 'react';
+
+export default class Chatroom extends Component{
+    render(){
+        return (<div id="chat-room">
+            <h2>here is chatting room</h2>
+        </div>)
+    }
+}
